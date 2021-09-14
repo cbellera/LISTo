0442c05e537cb1ee08c52482e4d236e9a747aaf9
cbellera <71798529+cbellera@users.noreply.github.com>
2021-09-14 12:40:58 -0300

    Update LISTo_v1.py

--- LISTo_v1.py
+++ LISTo_v1.py
@@ -70,7 +70,7 @@ st.sidebar.header('Upload your smiles')
 uploaded_file_1 = st.sidebar.file_uploader("Upload your smiles in a TXT file", type=["txt"])
 
 st.sidebar.markdown("""
-[Example TXT input file](https://raw.githubusercontent.com/cbellera/LUDe_v1/main/diclofenac_example.txt)
+[Example TXT input file](https://raw.githubusercontent.com/cbellera/LISTo_v1/main/diclofenac_example.txt)
 """)
 
 
