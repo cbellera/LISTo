66f892413fc83e20c07f3e34e34dde946b5e4c34
Carolina Bellera <71798529+cbellera@users.noreply.github.com>
2021-10-15 16:48:55 -0300

    Update LISTo_v1.py

--- LISTo_v1.py
+++ LISTo_v1.py
@@ -31,7 +31,7 @@ st.image(image)
 st.write("&nbsp[![Website](https://img.shields.io/badge/website-LIDeB-blue)](https://lideb.biol.unlp.edu.ar)&nbsp[![Twitter Follow](https://img.shields.io/twitter/follow/LIDeB_UNLP?style=social)](https://twitter.com/intent/follow?screen_name=LIDeB_UNLP)")
 st.subheader(":pushpin:" "About Us")
 st.markdown("We are a drug discovery team with an interest in the development of publicly available open-source customizable cheminformatics tools to be used in computer-assisted drug discovery. We belong to the Laboratory of Bioactive Research and Development (LIDeB) of the National University of La Plata (UNLP), Argentina. Our research group is focused on computer-guided drug repurposing and rational discovery of new drug candidates to treat epilepsy and neglected tropical diseases.")
-st.markdown(":computer:""**Web Site** " "<https://lideb.biol.unlp.edu.ar>")
+
 
 #---------------------------------#
 st.write("""
